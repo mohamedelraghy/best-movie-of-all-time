95fadd769c3711f6184223e5ff05b77efa8f98ca
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-05-11 20:49:45 +0300

    import Config Module

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -3,9 +3,13 @@ import { MulterModule } from '@nestjs/platform-express';
 
 import { MulterModuleConfig } from 'src/config/options/multer.config';
 import { UploadsService } from './uploads.service';
+import { ConfigModule } from 'src/config/config.module';
 
 @Module({
-  imports: [MulterModule.registerAsync({ useClass: MulterModuleConfig })],
+  imports: [
+    MulterModule.registerAsync({ useClass: MulterModuleConfig }),
+    ConfigModule.Deferred,
+  ],
   providers: [UploadsService],
 })
 export class UploadsModule {}
