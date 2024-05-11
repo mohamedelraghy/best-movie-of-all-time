effecb7153ca42b7e2fd1133ca950fc695262610
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-05-12 00:17:20 +0300

    some cleanup

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,15 +1,19 @@
 import {
   Controller,
+  HttpCode,
+  HttpStatus,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import * as csvParser from 'csv-parser';
+import { createReadStream } from 'fs';
 
 import { MoviesService } from './movies.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { createReadStream } from 'fs';
-import * as csvParser from 'csv-parser';
 import { UploadsService } from 'src/uploads/uploads.service';
 
 @ApiTags('movies')
@@ -21,25 +25,29 @@ export class MoviesController {
   ) {}
 
   @Post('sync')
+  @HttpCode(HttpStatus.OK)
   @ApiConsumes('multipart/form-data')
   @UseInterceptors(FileInterceptor('file'))
   async syncMovies(
     @UploadedFile()
     file: Express.Multer.File,
+    @Res() res: Response,
   ) {
     console.log({ file });
     // * 1- Get CSV file path
     const path = this.uploadService.saveFile(file);
     console.log({ path });
+
     // * 2- parse csv file
     const results = [];
     createReadStream(path)
       .pipe(csvParser())
       .on('data', (data) => results.push(data))
       .on('end', () => {
-        // Here you have the parsed CSV data in `results` array
         console.log({ results });
+        res.json(results);
       });
+
     // * 3- save movies to db
   }
 }
