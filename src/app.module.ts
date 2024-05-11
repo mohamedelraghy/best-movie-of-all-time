663e0e6a50f81150ee8915ee8560528a27ea03c5
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-05-11 18:00:22 +0300

    import core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { MoviesModule } from './movies/movies.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [MoviesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CoreModule, MoviesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
