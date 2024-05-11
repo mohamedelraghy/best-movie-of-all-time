62d426a9fdf333e2b882c1e427c01c66870aa55b
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-05-11 18:55:04 +0300

    setup and config main entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
+import { configure } from './config.main';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['error', 'debug', 'warn', 'verbose', 'log'],
+  });
+  const config = app.get(ConfigService);
+  const port = process.env.PORT || config.port || 8080;
+  configure(app, config);
+
+  await app.listen(port, () => {
+    Logger.verbose(
+      `🚀 Server listening on PORT:${port} | ${config.nodeEnv} | ${config.apiUrl}`,
+    );
+  });
 }
 bootstrap();
