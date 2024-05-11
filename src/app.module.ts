775bc52632fc7231edf2fee59f3b66795c31210f
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-05-11 20:40:37 +0300

    create uploads module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { MoviesModule } from './movies/movies.module';
 import { CoreModule } from './core/core.module';
 import { ConfigModule } from './config/config.module';
 import { ConfigModuleConfig } from './config/options/config.config';
+import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
   imports: [
     CoreModule,
     ConfigModule.forRootAsync(ConfigModule, { useClass: ConfigModuleConfig }),
     MoviesModule,
+    UploadsModule,
   ],
   controllers: [],
   providers: [],
