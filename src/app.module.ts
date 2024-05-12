1a5c054c25c456e0f890c967a569057260a1453a
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-05-13 00:04:30 +0300

    import mongoose Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { MoviesModule } from './movies/movies.module';
 import { CoreModule } from './core/core.module';
 import { ConfigModule } from './config/config.module';
 import { ConfigModuleConfig } from './config/options/config.config';
 import { UploadsModule } from './uploads/uploads.module';
+import { MongooseModuleConfig } from './config/options/database.config';
 
 @Module({
   imports: [
@@ -12,6 +14,10 @@ import { UploadsModule } from './uploads/uploads.module';
     ConfigModule.forRootAsync(ConfigModule, { useClass: ConfigModuleConfig }),
     MoviesModule,
     UploadsModule,
+    MongooseModule.forRootAsync({
+      useClass: MongooseModuleConfig,
+      imports: [ConfigModule.Deferred],
+    }),
   ],
   controllers: [],
   providers: [],
