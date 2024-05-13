7e8b11fb37467650ab699a3343d1c76b70c7f3fb
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-05-13 23:25:30 +0300

    create watchlist dto

---
+++ src/movies/dto/watchlist.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class WatchlistDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, example: 'movie' })
+  media_type: string = 'movie';
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({ type: Number, example: 597 })
+  media_id: number;
+
+  @IsBoolean()
+  @ApiProperty({ type: Boolean, example: true })
+  watchlist: boolean = true;
+}
