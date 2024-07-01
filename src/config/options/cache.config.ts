6da8b94c640d53c56414185a52c756641e031c4d
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-07-02 01:39:31 +0300

    create option factory for cache

---
+++ src/config/options/cache.config.ts
@@ -0,0 +1,23 @@
+import { CacheOptions, CacheOptionsFactory } from '@nestjs/cache-manager';
+import { Injectable } from '@nestjs/common';
+import { redisStore } from 'cache-manager-redis-store';
+
+@Injectable()
+export class CacheModuleConfig implements CacheOptionsFactory {
+  createCacheOptions():
+    | CacheOptions<Record<string, any>>
+    | Promise<CacheOptions<Record<string, any>>> {
+    return {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      store: async () =>
+        await redisStore({
+          // Store-specific configuration:
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+        }),
+    };
+  }
+}
