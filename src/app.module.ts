849f5eeaaa34ef093f4e2d9715ca2d574f7f9f75
Mohamed EL-Raghy <elraghy8@gmail.com>
2024-07-02 01:21:23 +0300

    register Redis Client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { redisStore } from 'cache-manager-redis-store';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { RedisClientOptions } from 'redis';
 
 import { MoviesModule } from './movies/movies.module';
 import { CoreModule } from './core/core.module';
@@ -7,6 +10,7 @@ import { ConfigModule } from './config/config.module';
 import { ConfigModuleConfig } from './config/options/config.config';
 import { UploadsModule } from './uploads/uploads.module';
 import { MongooseModuleConfig } from './config/options/database.config';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
@@ -18,8 +22,25 @@ import { MongooseModuleConfig } from './config/options/database.config';
       useClass: MongooseModuleConfig,
       imports: [ConfigModule.Deferred],
     }),
+    CacheModule.register<RedisClientOptions>({
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      store: async () =>
+        await redisStore({
+          // Store-specific configuration:
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+        }),
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
